c323d6259d8e71d94c97efe228e298236a8b6c5b
channamcgoud <mcgoud@gmail.com>
2016-01-08 16:25:26 -0500

    added ;

--- GitTest/src/FirstClass.java
+++ GitTest/src/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("This is the first class!");
 		System.out.println("Second Line");
-		System.out.println("Third Line")
+		System.out.println("Third Line");
 	}
 
 }
