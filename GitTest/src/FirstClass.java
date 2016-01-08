7671499bc041b5a65c0f8653ca052a5aa959f269
channamcgoud <mcgoud@gmail.com>
2016-01-08 16:23:54 -0500

    Update FirstClass.java

--- GitTest/src/FirstClass.java
+++ GitTest/src/FirstClass.java
@@ -3,7 +3,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("This is the first class!");
-		System.out.println("Second line");
+		System.out.println("Second Line");
+		System.out.println("Third Line")
 	}
 
 }
